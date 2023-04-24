373002d0a6d78cd5423c9e48664e09dc136dc593
alexandre21ssouza <alexandre2121ssouza@gmail.com>
2023-04-24 13:05:30 -0300

    Introdução da Classe Scanner e ArrayList

--- src/cartas/Jogo.java
+++ src/cartas/Jogo.java
@@ -1,10 +1,19 @@
 package cartas;
 
-
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Jogo {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+
+		ArrayList<Integer> maoJogador = new ArrayList<>();
+
+		System.out.println("Que começar a jogar? (sim) ou  (não) ");
+		String jogar = sc.next();
+
+		int soma = 0;
 	}
 }
