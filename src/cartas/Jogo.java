07764bcc1362eb8e273ccfde33e81eb3b82cc80b
alexandre21ssouza <alexandre2121ssouza@gmail.com>
2023-04-24 13:22:08 -0300

    Inserindo a condicional IF.

--- src/cartas/Jogo.java
+++ src/cartas/Jogo.java
@@ -19,6 +19,42 @@ public class Jogo {
 		while (jogar.equals("sim") && soma <= 20) {
 			System.out.println("pedir carta? ");
 			String carta = sc.next();
+			if (carta.equals("sim")) {
+
+				double sorte = Math.random();
+				int n = (int) (1 + sorte * (10 - 1));
+				maoJogador.add(n);
+				System.out.println(maoJogador);
+
+				for (int numero : maoJogador)
+					soma = soma + numero;
+				System.out.println("soma das cartas: " + soma);
+
+			} else if (carta.equals("não")) {
+				System.out.println("parei");
+
+			}
+
+			if (soma > 21) {
+
+				System.out.println("você perdeu!!");
+				maoJogador.clear();
+				soma = 0;
+				System.out.println("Jogar novamente? ");
+				jogar = sc.next();
+
+			} else if (soma == 21) {
+
+				System.out.println("você venceu!!");
+				maoJogador.clear();
+				soma = 0;
+
+				System.out.println("Jogar novamente? ");
+				jogar = sc.next();
+
+			}
+
 		}
+		System.out.println(" Obrigado por nos visitar! ");
 	}
 }
